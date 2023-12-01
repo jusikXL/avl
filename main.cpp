39091d8fe6a12eba8cb7f4ff993321ad6858390a
jusikXL <125464973+jusikXL@users.noreply.github.com>
2023-12-01 18:35:02 +0100

    feat: shuffle external function

--- main.cpp
+++ main.cpp
@@ -6,37 +6,42 @@
 #include <vector>
 using namespace std;
 
-int concatenate(const string &key, const int &i1, const int &i2)
-{
+int concatenate(const string& key, const int& i1, const int& i2) {
   return i1 + i2;
 };
 
-int main()
-{
+int main() {
   Ring<string, int> ring_first;
   Ring<string, int> ring_second;
 
-  vector<pair<string, int>> initialValues = {
-      {"a", 1},
-      {"b", 2},
-      {"c", 3},
-      {"d", 4},
-      {"e", 5},
-      {"f", 6},
-      {"g", 7},
-      {"h", 8},
-      {"i", 9},
-      {"j", 10},
+  vector<pair<string, int>> nodes_first = {
+      {"quattro", 4},
+      {"tre", 3},
+      {"due", 2},
+      {"uno", 1},
   };
 
-  for (const auto &pair : initialValues)
-  {
+  vector<pair<string, int>> nodes_second = {
+      {"bes", 5},
+      {"dort", 4},
+      {"uc", 3},
+      {"iki", 2},
+      {"bir", 1},
+  };
+
+  for (const auto& pair : nodes_first) {
     ring_first.push_front(pair.first, pair.second);
+  }
+
+  for (const auto& pair : nodes_second) {
     ring_second.push_front(pair.first, pair.second);
   }
 
-  Ring<string, int> ring_merged = join(ring_first, ring_second);
-  cout << ring_merged << endl;
+  cout << ring_first << endl;
+  cout << ring_second << endl;
+
+  Ring<string, int> ring_shuffled = shuffle(ring_first, 1, ring_second, 2, 3);
+  cout << ring_shuffled << endl;
 
   return 0;
 }
--- ring.hpp
+++ ring.hpp
@@ -5,34 +5,31 @@
 using namespace std;
 
 template <typename Key, typename Info>
-class Ring
-{
+class Ring {
 private:
   /////////////////////////////// NODE //////////////////////////////
-  class Node
-  {
+  class Node {
 
   public:
     Key key;
     Info info;
-    Node(const Key &k, const Info &i, Node *p = nullptr, Node *n = nullptr)
-        : key(k), info(i), _past(p ? p : this), _next(n ? n : this) {}
+    Node(const Key& k, const Info& i, Node* p = nullptr, Node* n = nullptr)
+      : key(k), info(i), _past(p ? p : this), _next(n ? n : this) { }
 
   private:
-    Node *_past;
-    Node *_next;
+    Node* _past;
+    Node* _next;
     friend class Ring<Key, Info>;
   };
   /////////////////////////////// NODE //////////////////////////////
 
-  Node *_sentinel;
+  Node* _sentinel;
   unsigned int _size;
 
-  bool is_sentinel(Node *node) const { return node == _sentinel; }
+  bool is_sentinel(Node* node) const { return node == _sentinel; }
 
-  Node *_push(Node *before, Node *after, const Key &key, const Info &info)
-  {
-    Node *new_node = new Node(key, info, before, after);
+  Node* _push(Node* before, Node* after, const Key& key, const Info& info) {
+    Node* new_node = new Node(key, info, before, after);
 
     before->_next = new_node;
     after->_past = new_node;
@@ -42,13 +39,12 @@ private:
     return new_node;
   }
 
-  Node *_pop(Node *node)
-  {
+  Node* _pop(Node* node) {
     if (is_sentinel(node))
       return _sentinel;
 
-    Node *to_delete = node;
-    Node *next_node = node->_next;
+    Node* to_delete = node;
+    Node* next_node = node->_next;
 
     node->_next->_past = node->_past;
     node->_past->_next = node->_next;
@@ -62,67 +58,58 @@ private:
 public:
   /////////////////////////////// ITERATORS //////////////////////////////
   template <typename Derived>
-  class IteratorBase
-  {
+  class IteratorBase {
   protected:
-    Node *_curr;
+    Node* _curr;
     friend class Ring<Key, Info>;
-    IteratorBase(Node *node) : _curr(node) {}
+    IteratorBase(Node* node) : _curr(node) { }
 
   public:
-    Derived &operator++()
-    {
+    Derived& operator++() {
       _curr = _curr->_next;
-      return static_cast<Derived &>(*this);
+      return static_cast<Derived&>(*this);
     }
 
-    Derived operator++(int)
-    {
-      Derived temp = static_cast<Derived &>(*this);
+    Derived operator++(int) {
+      Derived temp = static_cast<Derived&>(*this);
       ++(*this);
       return temp;
     }
 
-    Derived &operator--()
-    {
+    Derived& operator--() {
       _curr = _curr->_past;
-      return static_cast<Derived &>(*this);
+      return static_cast<Derived&>(*this);
     }
 
-    Derived operator--(int)
-    {
-      Derived temp = static_cast<Derived &>(*this);
+    Derived operator--(int) {
+      Derived temp = static_cast<Derived&>(*this);
       --(*this);
       return temp;
     }
 
-    bool operator==(const Derived &other) const
-    {
+    bool operator==(const Derived& other) const {
       return _curr == other._curr;
     }
 
-    bool operator!=(const Derived &other) const
-    {
+    bool operator!=(const Derived& other) const {
       return _curr != other._curr;
     }
   };
 
-  class Iterator : public IteratorBase<Iterator>
-  {
+  class Iterator : public IteratorBase<Iterator> {
   public:
     using IteratorBase<Iterator>::IteratorBase;
 
-    Key &key() { return this->_curr->key; }
-    Info &info() { return this->_curr->info; }
+    Key& key() { return this->_curr->key; }
+    Info& info() { return this->_curr->info; }
   };
 
-  class ConstIterator : public IteratorBase<ConstIterator>
-  {
+  class ConstIterator : public IteratorBase<ConstIterator> {
   public:
     using IteratorBase<ConstIterator>::IteratorBase;
 
-    const Key &key() const { return this->_curr->key; }
-    const Info &info() const { return this->_curr->info; }
+    const Key& key() const { return this->_curr->key; }
+    const Info& info() const { return this->_curr->info; }
   };
 
   Iterator begin() { return Iterator(_sentinel->_next); }
@@ -133,12 +120,9 @@ public:
   /////////////////////////////// ITERATORS //////////////////////////////
 
 private:
-  Iterator _find(const Key &search_key, Iterator from, Iterator to)
-  {
-    for (Iterator it = from; it != to; ++it)
-    {
-      if (it.key() == search_key)
-      {
+  Iterator _find(const Key& search_key, Iterator from, Iterator to) {
+    for (Iterator it = from; it != to; ++it) {
+      if (it.key() == search_key) {
         return it;
       }
     }
@@ -147,25 +131,21 @@ private:
   }
 
 public:
-  Ring() : _sentinel(new Node(Key{}, Info{})), _size(0) {}
+  Ring() : _sentinel(new Node(Key{}, Info{})), _size(0) { }
 
-  ~Ring()
-  {
+  ~Ring() {
     clear();
     delete _sentinel;
   }
 
-  Ring &operator=(const Ring &src)
-  {
-    if (this != &src)
-    {
+  Ring& operator=(const Ring& src) {
+    if (this != &src) {
       clear();
 
       ConstIterator it_last = --(src.cend());
       ConstIterator it_sentinel = --(src.cbegin());
 
-      for (ConstIterator it = it_last; it != it_sentinel; it--)
-      {
+      for (ConstIterator it = it_last; it != it_sentinel; it--) {
         push_front(it.key(), it.info());
       }
     }
@@ -173,61 +153,53 @@ public:
     return *this;
   }
 
-  Ring(const Ring &src) : Ring() { *this = src; }
+  Ring(const Ring& src) : Ring() { *this = src; }
 
-  Iterator push_front(const Key &key, const Info &info)
-  {
+  unsigned int size() {
+    return _size;
+  }
+
+  Iterator push_front(const Key& key, const Info& info) {
     return Iterator(_push(_sentinel, _sentinel->_next, key, info));
   }
 
-  Iterator pop_front()
-  {
+  Iterator pop_front() {
     return Iterator(_pop(_sentinel->_next));
   }
 
-  Iterator erase(Iterator position)
-  {
+  Iterator erase(Iterator position) {
     return Iterator(_pop(position._curr));
   }
 
-  Iterator insert(Iterator position, const Key &key, const Info &info)
-  {
+  Iterator insert(Iterator position, const Key& key, const Info& info) {
     return Iterator(_push(position._curr->_past, position._curr, key, info));
   }
 
-  void clear()
-  {
-    while (_size)
-    {
+  void clear() {
+    while (_size) {
       _pop(_sentinel->_next);
     }
   }
 
-  Iterator find(const Key &search_key)
-  {
+  Iterator find(const Key& search_key) {
     return _find(search_key, begin(), end());
   }
 };
 
 template <typename Key, typename Info>
-ostream &operator<<(ostream &os, const Ring<Key, Info> &ring)
-{
-  for (typename Ring<Key, Info>::ConstIterator it = ring.cbegin(); it != ring.cend(); ++it)
-  {
+ostream& operator<<(ostream& os, const Ring<Key, Info>& ring) {
+  for (typename Ring<Key, Info>::ConstIterator it = ring.cbegin(); it != ring.cend(); ++it) {
     os << it.key() << " : " << it.info() << endl;
   }
   return os;
 }
 
 template <typename Key, typename Info>
-Ring<Key, Info> filter(const Ring<Key, Info> &src, bool (*pred)(const Key &))
-{
+Ring<Key, Info> filter(const Ring<Key, Info>& src, bool (*pred)(const Key&)) {
   Ring<Key, Info> result;
 
-  for (typename Ring<Key, Info>::ConstIterator it = --(src.cend()); it != --(src.cbegin()); it--)
-  {
-    if (pred(it.key()))
-    {
+  for (typename Ring<Key, Info>::ConstIterator it = --(src.cend()); it != --(src.cbegin()); it--) {
+    if (pred(it.key())) {
       result.push_front(it.key(), it.info());
     }
   }
@@ -236,21 +208,16 @@ Ring<Key, Info> filter(const Ring<Key, Info> &src, bool (*pred)(const Key &))
 }
 
 template <typename Key, typename Info>
-Ring<Key, Info> unique(const Ring<Key, Info> &src, Info(aggregate)(const Key &, const Info &, const Info &))
-{
+Ring<Key, Info> unique(const Ring<Key, Info>& src, Info(aggregate)(const Key&, const Info&, const Info&)) {
   Ring<Key, Info> result;
 
-  for (typename Ring<Key, Info>::ConstIterator it = --(src.cend()); it != --(src.cbegin()); it--)
-  {
+  for (typename Ring<Key, Info>::ConstIterator it = --(src.cend()); it != --(src.cbegin()); it--) {
     typename Ring<Key, Info>::Iterator it_res = result.find(it.key());
 
-    if (it_res != result.end())
-    {
+    if (it_res != result.end()) {
       // if key's been added to result previously
       it_res.info() = aggregate(it.key(), it_res.info(), it.info());
-    }
-    else
-    {
+    } else {
       result.push_front(it.key(), it.info());
     }
   }
@@ -259,27 +226,68 @@ Ring<Key, Info> unique(const Ring<Key, Info> &src, Info(aggregate)(const Key &, 
 }
 
 template <typename Key, typename Info>
-Info _concatenate_info(const Key &, const Info &i1, const Info &i2)
-{
+Info _concatenate_info(const Key&, const Info& i1, const Info& i2) {
   return i1 + i2;
 }
 
 template <typename Key, typename Info>
-Ring<Key, Info> join(const Ring<Key, Info> &first, const Ring<Key, Info> &second)
-{
+Ring<Key, Info> join(const Ring<Key, Info>& first, const Ring<Key, Info>& second) {
   Ring<Key, Info> result;
 
-  for (typename Ring<Key, Info>::ConstIterator it = --(first.cend()); it != --(first.cbegin()); it--)
-  {
+  for (typename Ring<Key, Info>::ConstIterator it = --(first.cend()); it != --(first.cbegin()); it--) {
     result.push_front(it.key(), it.info());
   }
 
-  for (typename Ring<Key, Info>::ConstIterator it = --(second.cend()); it != --(second.cbegin()); it--)
-  {
+  for (typename Ring<Key, Info>::ConstIterator it = --(second.cend()); it != --(second.cbegin()); it--) {
     result.push_front(it.key(), it.info());
   }
 
   return unique(result, _concatenate_info<Key, Info>);
+}
+
+template <typename Key, typename Info>
+Ring<Key, Info> reverse(const Ring<Key, Info>& ring) {
+  Ring<Key, Info> reversed;
+
+  for (typename Ring<Key, Info>::ConstIterator it = ring.cbegin(); it != ring.cend(); it++) {
+    reversed.push_front(it.key(), it.info());
+  }
+
+  return reversed;
+}
+
+
+template <typename Key, typename Info>
+Ring<Key, Info> shuffle(
+  const Ring<Key, Info>& first, unsigned int fcnt,
+  const Ring<Key, Info>& second, unsigned int scnt,
+  unsigned int reps) {
+  Ring<Key, Info> result;
+
+  typename Ring<Key, Info>::ConstIterator it_first = first.cbegin();
+  typename Ring<Key, Info>::ConstIterator it_second = second.cbegin();
+
+  for (unsigned int rep = 0; rep < reps; rep++) {
+    for (unsigned int i = 0; i < fcnt; i++) {
+      result.push_front(it_first.key(), it_first.info());
+
+      it_first++;
+      if (it_first == first.cend()) {
+        it_first = first.cbegin(); // if _sentinel is reached reset the iterator
+      }
+    }
+
+    for (unsigned int i = 0; i < scnt; i++) {
+      result.push_front(it_second.key(), it_second.info());
+
+      it_second++;
+      if (it_second == second.cend()) {
+        it_second = second.cbegin();
+      }
+    }
+  }
+
+  return reverse(result);
 }
 
 #endif // RING_HPP
